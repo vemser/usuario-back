999c57ae49e460ca5aee46f33a7d0fa7485f3811
Luiz Martins <luiiz.martiins@outlook.com>
2022-12-20 23:51:11 -0300

    fix: Adicionado permissionamento no SecurityConfiguration

--- src/main/java/br/com/dbc/usuarioapi/security/SecurityConfiguration.java
+++ src/main/java/br/com/dbc/usuarioapi/security/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package br.com.dbc.usuarioapi.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -28,53 +29,11 @@ public class SecurityConfiguration {
                 .frameOptions().disable()
                 .and().cors()
                 .and().csrf().disable()
-//                .authorizeHttpRequests((auth) -> auth.antMatchers("/", "/login", "/login-forgot-password").permitAll()
-                        .authorizeHttpRequests((auth) -> auth.antMatchers("/**").permitAll()
-
-//                                .antMatchers(HttpMethod.PUT, "/foto/upload-image/**").hasRole("ADMIN")
-//                                .antMatchers(HttpMethod.PUT, "/foto/upload-image-perfil").hasAnyRole("INSTRUTOR", "GESTAO_DE_PESSOAS", "ADMIN")
-//                                .antMatchers(HttpMethod.GET,"/usuario/logged-user").hasAnyRole("INSTRUTOR", "GESTAO_DE_PESSOAS", "ADMIN")
-//                                .antMatchers(HttpMethod.GET, "/usuario").hasRole("ADMIN")
-//                                .antMatchers(HttpMethod.POST, "/usuario").hasRole("ADMIN")
-//                                .antMatchers(HttpMethod.DELETE, "/usuario/**").hasRole("ADMIN")
-//                                .antMatchers("/usuario/enable-disable/**").hasRole("ADMIN")
-//                                .antMatchers("/usuario/update-cadastro/**").hasRole("ADMIN")
-//
-//                                .antMatchers(HttpMethod.GET, "/edicao/listar").hasAnyRole("GESTAO_DE_PESSOAS", "INSTRUTOR")
-//                                .antMatchers(HttpMethod.GET, "/edicao/listar-com-etapa").hasAnyRole("GESTAO_DE_PESSOAS", "INSTRUTOR")
-//                                .antMatchers(HttpMethod.GET, "/edicao/calendario-geral").hasAnyRole("GESTAO_DE_PESSOAS", "INSTRUTOR")
-//                                .antMatchers(HttpMethod.GET, "/edicao/calendario-edicao/**").hasAnyRole("GESTAO_DE_PESSOAS", "INSTRUTOR")
-//                                .antMatchers(HttpMethod.POST, "/edicao").hasRole("GESTAO_DE_PESSOAS")
-//                                .antMatchers(HttpMethod.DELETE, "/edicao/**").hasRole("GESTAO_DE_PESSOAS")
-//                                .antMatchers("/edicao/**").hasRole("GESTAO_DE_PESSOAS")
-//
-//                                .antMatchers(HttpMethod.GET, "/etapa").hasAnyRole("GESTAO_DE_PESSOAS", "INSTRUTOR")
-//                                .antMatchers(HttpMethod.GET, "/etapa/**").hasAnyRole("GESTAO_DE_PESSOAS", "INSTRUTOR")
-//                                .antMatchers(HttpMethod.POST, "/etapa").hasRole("GESTAO_DE_PESSOAS")
-//                                .antMatchers(HttpMethod.DELETE, "/etapa/**").hasRole("GESTAO_DE_PESSOAS")
-//                                .antMatchers("/etapa/**").hasRole("GESTAO_DE_PESSOAS")
-//
-//                                .antMatchers(HttpMethod.GET, "/processo").hasAnyRole("GESTAO_DE_PESSOAS", "INSTRUTOR")
-//                                .antMatchers(HttpMethod.GET, "/processo/**").hasAnyRole("GESTAO_DE_PESSOAS", "INSTRUTOR")
-//                                .antMatchers(HttpMethod.POST, "/processo").hasRole("GESTAO_DE_PESSOAS")
-//                                .antMatchers(HttpMethod.DELETE, "/processo/**").hasRole("GESTAO_DE_PESSOAS")
-//                                .antMatchers("/processo/**").hasRole("GESTAO_DE_PESSOAS")
-//
-//                                .antMatchers(HttpMethod.GET, "/area-envolvida").hasRole("GESTAO_DE_PESSOAS")
-//                                .antMatchers(HttpMethod.POST, "/area-envolvida").hasRole("GESTAO_DE_PESSOAS")
-//                                .antMatchers(HttpMethod.DELETE, "/area-envolvida").hasRole("GESTAO_DE_PESSOAS")
-//                                .antMatchers("/area-envolvida/**").hasRole("GESTAO_DE_PESSOAS")
-//
-//                                .antMatchers(HttpMethod.GET, "/responsavel").hasRole("GESTAO_DE_PESSOAS")
-//                                .antMatchers(HttpMethod.POST, "/responsavel").hasRole("GESTAO_DE_PESSOAS")
-//                                .antMatchers(HttpMethod.DELETE, "/responsavel").hasRole("GESTAO_DE_PESSOAS")
-//                                .antMatchers("/responsavel/**").hasRole("GESTAO_DE_PESSOAS")
-//
-//                                .antMatchers(HttpMethod.GET, "/dia-nao-util").hasAnyRole("GESTAO_DE_PESSOAS", "INSTRUTOR")
-//                                .antMatchers(HttpMethod.POST, "/dia-nao-util").hasRole("GESTAO_DE_PESSOAS")
-//                                .antMatchers(HttpMethod.DELETE, "/dia-nao-util").hasRole("GESTAO_DE_PESSOAS")
-//                                .antMatchers("/dia-nao-util/**").hasRole("GESTAO_DE_PESSOAS")
-
+                .authorizeHttpRequests((auth) -> auth.antMatchers("/", "/usuario/login").permitAll()
+                        .antMatchers(HttpMethod.PUT,"/usuario/update-cargos/**").hasAnyRole("ADMIN")
+                        .antMatchers(HttpMethod.GET, "/usuario").hasRole("ADMIN")
+                        .antMatchers(HttpMethod.POST, "/usuario").hasRole("ADMIN")
+                        .antMatchers(HttpMethod.DELETE, "/usuario/**").hasRole("ADMIN")
                                 .anyRequest().authenticated()
                 );
         http.addFilterBefore(new TokenAuthenticationFilter(tokenService), UsernamePasswordAuthenticationFilter.class);
