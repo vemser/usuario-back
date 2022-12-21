f84f62ca95916712e0e5f0f103c10112e5791cb3
Luiz Martins <luiiz.martiins@outlook.com>
2022-12-20 23:31:45 -0300

    fix: Comentada classe de tests para subir no jenkins

--- src/test/java/br/com/dbc/usuarioapi/UsuarioApplicationTests.java
+++ src/test/java/br/com/dbc/usuarioapi/UsuarioApplicationTests.java
@@ -1,13 +1,13 @@
-package br.com.dbc.usuarioapi;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class UsuarioApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package br.com.dbc.usuarioapi;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class UsuarioApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
